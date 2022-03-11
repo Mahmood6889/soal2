1560d213523f4925bc5b89b61735befa29f58fdb
mahmood6889 <mahmood_sh6889@yahoo.com>
2022-03-11 14:03:01 +0330

    ygyfv

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int number;
+double sum = 0;
+int counter = -1;
+
+do
+{
+    System.Console.WriteLine("add number:");
+    number = Convert.ToInt32(Console.ReadLine());
+
+    sum = sum + number;
+    counter++;
+
+} while (number != 0);
+
+System.Console.WriteLine(sum/counter);
